68d98ea845ff85c50aa0bd29ef53c1c76f0ca6aa
Puneet Naik <naikpuneet09@gmail.com>
2026-07-02 22:55:21 +0530

    commit

---
+++ backend/modules/service.js
@@ -0,0 +1,38 @@
+const mongoose= require("mongoose");
+
+const personSchema = new mongoose.Schema({
+    name:{
+        type : String,
+        require:true
+    },
+    age:{
+        type:Number,
+        require:true
+    },
+    work:{
+        type:String,
+        enum:["chef", "waiter", "manager"], 
+        require:true
+    },
+    mobile:{
+        type:String,
+        require:true
+    },
+
+    email:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    address:{
+        type:String,
+        require:true
+    },
+salary:{
+        type:Number,
+        require:true
+    }
+
+})
+
+// module.exports= mongoose.model("Person",personSchema);
