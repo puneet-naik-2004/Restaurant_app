92643fa0db29e926616926759e2bf8c98d10c7bc
Puneet Naik <naikpuneet09@gmail.com>
2026-07-05 22:43:38 +0530

    add if

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -28,6 +28,9 @@ exports.updateService=async(req,res)=>{
     try{
          const data = await service.findByIdAndUpdate(req.params.id,req.body)
          res.status(200).json(data)
+         if(!service){
+            res.status(404).json({error:"not found"})
+         }
     }catch(error){
          res.status(400).json(error)
          console.log(error);
@@ -39,6 +42,8 @@ exports.deleteService=async(req,res)=>{
     try{
   const data= await service.findByIdAndDelete(req.params.id)
   res.status(200).json(data)
+
+  
     }catch(error){
         res.status(400).json(error)
         console.log(error)
